406db3d8dac8c3de3467fc9aa88252d04ad367b1
Xinqi Mu <hi@muxinqi.com>
2023-12-08 12:22:20 +0800

    Fix lint warning `no-var-requires`

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -1,6 +1,9 @@
 import clsx from 'clsx';
 import Heading from '@theme/Heading';
 import styles from './styles.module.css';
+import undraw_docusaurus_mountain from '@site/static/img/undraw_docusaurus_mountain.svg'
+import undraw_docusaurus_tree from '@site/static/img/undraw_docusaurus_tree.svg'
+import undraw_docusaurus_react from '@site/static/img/undraw_docusaurus_react.svg'
 
 type FeatureItem = {
   title: string;
@@ -11,7 +14,7 @@ type FeatureItem = {
 const FeatureList: FeatureItem[] = [
   {
     title: 'Easy to Use',
-    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+    Svg: undraw_docusaurus_mountain,
     description: (
       <>
         Docusaurus was designed from the ground up to be easily installed and
@@ -21,7 +24,7 @@ const FeatureList: FeatureItem[] = [
   },
   {
     title: 'Focus on What Matters',
-    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
+    Svg: undraw_docusaurus_tree,
     description: (
       <>
         Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
@@ -31,7 +34,7 @@ const FeatureList: FeatureItem[] = [
   },
   {
     title: 'Powered by React',
-    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+    Svg: undraw_docusaurus_react,
     description: (
       <>
         Extend or customize your website layout by reusing React. Docusaurus can
