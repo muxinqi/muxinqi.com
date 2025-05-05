e85e5fccbc0725d5592eac890c940d51a3c13e73
Xinqi Mu <hi@muxinqi.com>
2025-05-05 00:46:58 -0400

    Add TailwindCSS Integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,17 @@ import { defineConfig } from 'astro/config';
 
 import cloudflare from '@astrojs/cloudflare';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
   adapter: cloudflare({
     platformProxy: {
       enabled: true
     }
-  })
+  }),
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
