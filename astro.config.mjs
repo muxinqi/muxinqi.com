cfc97319151ad440b0ce4340ededa775b7999bea
Xinqi Mu <hi@muxinqi.com>
2025-05-08 12:25:12 +0800

    Change to static website by removing cloudflare adapter (#19)
    * Remove cloudflare adapter
    
    * Update wrangler config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,9 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
-import cloudflare from '@astrojs/cloudflare';
-
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: cloudflare({
-    platformProxy: {
-      enabled: true
-    }
-  }),
-
   vite: {
     plugins: [tailwindcss()]
   }
